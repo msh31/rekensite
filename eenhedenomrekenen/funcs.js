38db26dd67e667faaaaf639e36cebf9693ccb069
Marco <mshendriks03@gmail.com>
2024-09-26 09:20:43 +0200

    sync with master
    [ MANUAL ]

--- eenhedenomrekenen/funcs.js
+++ eenhedenomrekenen/funcs.js
@@ -1,7 +1,23 @@
-function checkButton() {
-    alert("hi!");
+const units = ["mm", "cm", "dm", "m", "dam", "hm", "km"];
+
+let dimension = document.getElementById("selectionMenu");
+let fieldInput = document.getElementById("inputField");
+let fieldAnswer = document.getElementById("answerField");
+let unitIdentity = document.getElementById("unitIdentifier");
+let AnswerUnitIdentity = document.getElementById("answerUnitIdentifier");
+
+function nieuw() {
+    let dimvalue = dimension.value;
+
+    if(dimvalue == 0) {
+        alert("Kies eerst een dimensie!");
+    } else {
+        let randomNumber = Math.floor(Math.random() * 7);
+        //console.log("Random Number: " + randomNumber);
+        unitIdentity..innerHTML = units[randomNumber] + "<sup>" + dimvalue + "</sup>";
+    }
 }
 
-function refreshButton() {
-    alert("bye!");
+function check() {
+    alert("Hallo " + naam);
 }

1f67ee042cfa21dba8e56ece3cbc208c6116d042
Marco <mshendriks03@gmail.com>
2024-09-30 12:14:02 +0200

    User Experience Update

--- eenhedenomrekenen/funcs.js
+++ eenhedenomrekenen/funcs.js
@@ -1,23 +1,31 @@
 const units = ["mm", "cm", "dm", "m", "dam", "hm", "km"];
 
-let dimension = document.getElementById("selectionMenu");
-let fieldInput = document.getElementById("inputField");
-let fieldAnswer = document.getElementById("answerField");
-let unitIdentity = document.getElementById("unitIdentifier");
-let AnswerUnitIdentity = document.getElementById("answerUnitIdentifier");
+
+let selectionMenu = document.getElementById("selectionMenu");
+let inputField = document.getElementById("inputField");
+let answerField = document.getElementById("answerField");
+let unitIdentifier = document.getElementById("unitIdentifier");
+let answerUnitIdentifier = document.getElementById("answerUnitIdentifier");
 
 function nieuw() {
-    let dimvalue = dimension.value;
+    answerField.value = "";
+    let dimvalue = selectionMenu.value;
+    let randomNumber = Math.floor(Math.random() * 7);
+    let secondRandomNumber = Math.floor(Math.random() * 7);
+    let inputFieldDecimal = (Math.random() * 1000).toFixed(3);
 
-    if(dimvalue == 0) {
-        alert("Kies eerst een dimensie!");
-    } else {
-        let randomNumber = Math.floor(Math.random() * 7);
-        //console.log("Random Number: " + randomNumber);
-        unitIdentity.innerHTML = units[randomNumber] + "<sup>" + dimvalue + "</sup>";
+    if ( dimvalue == 1 ) { 
+        unitIdentifier.innerHTML = units[randomNumber] 
+        answerUnitIdentifier.innerHTML = units[secondRandomNumber]
     }
+    else { 
+        unitIdentifier.innerHTML = units[randomNumber] + "<sup>" + dimvalue + "</sup>"; 
+        answerUnitIdentifier.innerHTML = units[secondRandomNumber] + "<sup>" + dimvalue + "</sup>";
+    }
+
+    inputField.value = inputFieldDecimal;
 }
 
 function check() {
-    alert("Hallo " + naam);
+    alert("hi!");
 }
